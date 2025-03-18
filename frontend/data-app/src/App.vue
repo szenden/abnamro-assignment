0bf35b464a26669561e2b17e6492600c4b03a61c
Felix Aweh <aweh.felix@hotmail.com>
2025-03-18 18:02:05 +0100

    Refactor App.vue and remove unused components
    - Cleaned up App.vue by removing commented-out code and the HelloWorld component.
    - Deleted TheWelcome, WelcomeItem, and AboutView components as they are no longer needed.
    - Updated HomeView.vue to replace TheWelcome component with a simple heading.

--- frontend/data-app/src/App.vue
+++ frontend/data-app/src/App.vue
@@ -1,26 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 // import HierarchyChart from './components/HierarchyChart.vue'
 import HierarchyChartWithPopup from './components/HierarchyChartWithPopup.vue'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView /> -->
-
   <div>
     <h1>Graph Hierarchy</h1>
     <HierarchyChartWithPopup />
